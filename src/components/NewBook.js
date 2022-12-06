b5734336074d7a0e73f144ec2aa2330810ec477a
shella12 <ayeshaarshad4567@gmail.com>
2022-12-06 13:30:53 +0500

    Fix Select error

--- src/components/NewBook.js
+++ src/components/NewBook.js
@@ -26,23 +26,26 @@ function NewBook() {
   };
   const changeColor = (e) => {
     e.target.style.color = '#000';
+    setCategories(e.target.value);
   };
   return (
     <form>
+      {console.log(categories)}
       <input className="Lesson-input" type="text" name="title" placeholder="Book title" value={title} onChange={(e) => setTitle(e.target.value)} />
       <input className="Lesson-input" type="text" name="author" placeholder="Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-      <select className="Lesson-Select" name="categories" id="categories" onChange={(e) => changeColor(e)}>
-        <option value="" disabled selected hidden>Category</option>
-        <option value="Sci-Fi" onSelect={(e) => setCategories(e.target.value)}>Sci-Fi</option>
-        <option value="Romance" onSelect={(e) => setCategories(e.target.value)}>Romance</option>
-        <option value="Drama" onSelect={(e) => setCategories(e.target.value)}>Drama</option>
-        <option value="Fiction" onSelect={(e) => setCategories(e.target.value)}>Fiction</option>
-        <option value="Mystery" onSelect={(e) => setCategories(e.target.value)}>Mystery</option>
-        <option value="Biography" onSelect={(e) => setCategories(e.target.value)}>Biography</option>
-        <option value="Autobiography" onSelect={(e) => setCategories(e.target.value)}>Autobiography</option>
-        <option value="Comedy" onSelect={(e) => setCategories(e.target.value)}>Comedy</option>
-        <option value="Horror" onSelect={(e) => setCategories(e.target.value)}>Horror</option>
-        <option value="Thriler" onSelect={(e) => setCategories(e.target.value)}>Thriler</option>
+      {/* eslint-disable-next-line react/jsx-curly-brace-presence */}
+      <select defaultValue={'DEFAULT'} className="Lesson-Select" name="categories" id="categories" onChange={(e) => changeColor(e)}>
+        <option value="DEFAULT" disabled>Category</option>
+        <option value="Sci-Fi">Sci-Fi</option>
+        <option value="Romance">Romance</option>
+        <option value="Drama">Drama</option>
+        <option value="Fiction">Fiction</option>
+        <option value="Mystery">Mystery</option>
+        <option value="Biography">Biography</option>
+        <option value="Autobiography">Autobiography</option>
+        <option value="Comedy">Comedy</option>
+        <option value="Horror">Horror</option>
+        <option value="Thriler">Thriler</option>
       </select>
       <button className="add-book-btn" type="submit" onClick={(event) => onClickHandler(event)}>submit</button>
     </form>
